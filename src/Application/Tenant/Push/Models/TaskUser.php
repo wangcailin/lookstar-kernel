22d18426b1849c5d73034e3bad925f807296cd5a
王彩霖 <root@Bluedot-Development-Cailin>
2023-04-04 11:19:21 +0800

    feat(推送): 增加推送Model

--- src/Application/Tenant/Push/Models/TaskUser.php
+++ src/Application/Tenant/Push/Models/TaskUser.php
@@ -9,6 +9,9 @@ class TaskUser extends Model
     protected $connection = 'data_warehouse';
     protected $table = 'ads_push_task_user';
 
+    public $incrementing = false;
+    protected $primaryKey = 'value';
+
     protected $fillable = [
         'task_id',
         'task_type',
