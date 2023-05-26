26ad0c6199126bf8290175731f05e3abbdd1bc88
王彩霖 <root@Bluedot-Development-Cailin>
2023-05-26 16:36:27 +0800

    fix(Logging): 解决Log问题

--- src/Logging/AnalyticsFormatter.php
+++ src/Logging/AnalyticsFormatter.php
@@ -3,12 +3,13 @@
 namespace LookstarKernel\Logging;
 
 use Monolog\Formatter\JsonFormatter;
+use Monolog\LogRecord;
 
 class AnalyticsFormatter extends JsonFormatter
 {
-    public function format(array $record): string
+    public function format(LogRecord $record): string
     {
-        $normalized = $this->normalize($record);
+        $normalized = $this->normalize($record->toArray());
         if (isset($normalized['context'])) {
             $normalized = array_merge($normalized, $normalized['context']);
             unset($normalized['context']);
