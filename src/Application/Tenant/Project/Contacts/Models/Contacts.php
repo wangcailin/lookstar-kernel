fa15e8ecdf727016a69d7d2cd99f2959b516994c
dongbo.sang <15225140805@163.com>
2023-08-18 16:03:03 +0800

    feat(project): 项目联系人邮件配置修改

---
+++ src/Application/Tenant/Project/Contacts/Models/Contacts.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LookstarKernel\Application\Tenant\Project\Contacts\Models;
+
+use LookstarKernel\Support\Eloquent\TenantModel as Model;
+
+class Contacts extends Model
+{
+    protected $table = 'tenant_forms_contacts';
+}
