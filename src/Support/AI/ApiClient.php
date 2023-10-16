80065539bf9a428524756587be4fc399260b81f4
dongbo.sang <15225140805@163.com>
2023-10-16 11:00:23 +0800

    to(ai): 修改apiClient

--- src/Support/AI/ApiClient.php
+++ src/Support/AI/ApiClient.php
@@ -37,21 +37,19 @@ class ApiClient
             self::$domain . $path,
             $data
         );
+        Log::info('AI保存或者更新文件，请求的数据是' . json_encode($data));
         if ($response->successful()) {
             return $response->json();
         } else {
             $statusCode = $response->status(); // 获取 HTTP 状态码
             $body = $response->body(); // 获取响应内容
-            Log::info('******************************************************************');
-            Log::info($body);
-            Log::info($statusCode);
-            Log::info('------------------------------------------------------------------');
             return false;
         }
     }
 
     public static function delete($path, $data, $header = [])
     {
+        Log::info('AI删除源文件，请求的数据是' . json_encode($data));
         $response = Http::timeout(60)->withHeaders($header ?: self::$header)->delete(
             self::$domain . $path,
             $data
