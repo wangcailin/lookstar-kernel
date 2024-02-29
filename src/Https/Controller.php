a5a6d5c5f95c79c0851e89a588be21c32edcd079
root <root@Bluedot-AI>
2024-02-29 14:24:48 +0800

    fix(Controller): 重构Controller

--- src/Https/Controller.php
+++ src/Https/Controller.php
@@ -11,13 +11,11 @@ use Illuminate\Support\Facades\Validator;
 
 class Controller extends ComposerController
 {
+
     /**
-     * 查询的时候是否启用 部门权限字段
+     * 数据是否绑定当前管理员部门权限ID
      */
-    public $departmentPermission = true;
-    public $departmentIdField = 'role_id';
-    public $departmentIdFieldIsJson = 0;
-    public $originalModel;
+    public $authRoleId = true;
 
     /**
      * 给 filter 添加部门权限
@@ -26,7 +24,28 @@ class Controller extends ComposerController
      */
     public function beforeBuildFilter()
     {
-        $this->queryDepartmentPermission();
+        $this->queryAuthRoleId();
+    }
+
+    public function queryAuthRoleId()
+    {
+        if ($this->authRoleId) {
+            $authRoleId = Auth::guard($this->guard)->roles[0]['id'];
+            $this->model->where('auth_role_id', $authRoleId);
+        }
+    }
+
+    /**
+     * 创建数据 注入后台用户ID auth_role_id
+     *
+     * @return void
+     */
+    public function createAuthRoleId()
+    {
+        if ($this->guard) {
+            $authRoleId = Auth::guard($this->guard)->roles[0]['id'];
+            $this->data['auth_role_id'] = $authRoleId;
+        }
     }
 
     /**
@@ -39,75 +58,8 @@ class Controller extends ComposerController
         if ($this->authUserId) {
             $this->createAuthUserId();
         }
-        if ($this->departmentPermission) {
-            $this->createDepartmentId();
-        }
-    }
-
-    /**
-     * 更新数据 前置方法
-     *
-     * @return void
-     */
-    public function beforeUpdate()
-    {
-        $user = $this->getCurrentUser();
-        if (!$user->is_admin) {
-            $departmentId = $this->getUserDepartmentId($user);
-            if ($this->departmentIdFieldIsJson) {
-                $this->model = $this->model->whereJsonContains($this->departmentIdField, $departmentId);
-            } else {
-                $this->model = $this->model->where($this->departmentIdField, $departmentId);
-            }
-        }
-    }
-
-    /**
-     * 删除数据 核心方法 验证部门
-     *
-     * @return void
-     */
-    public function beforeDelete()
-    {
-        $user = $this->getCurrentUser();
-        if (!$user->is_admin) {
-            $departmentId = $this->getUserDepartmentId($user);
-            if ($this->departmentIdFieldIsJson) {
-                $this->model = $this->model->whereJsonContains($this->departmentIdField, $departmentId);
-            } else {
-                $this->model = $this->model->where($this->departmentIdField, $departmentId);
-            }
-        }
-    }
-
-    /**
-     * 获取单个数据 格式化数据
-     *
-     * @return void
-     */
-    public function getDataTransform()
-    {
-        if ($this->authUserId) {
-            $this->createAuthUserId();
-        }
-        if ($this->departmentPermission) {
-            $this->createDepartmentId();
-            $this->queryDepartmentPermission();
-        }
-    }
-
-
-    /**
-     * 创建数据时，添加部门ID
-     *
-     * @return void
-     */
-    public function createDepartmentId()
-    {
-        if ($this->departmentIdFieldIsJson) {
-            $this->data[$this->departmentIdField][] = $this->getUserDepartmentId();
-        } else {
-            $this->data[$this->departmentIdField] = $this->getUserDepartmentId();
+        if ($this->authRoleId) {
+            $this->createAuthRoleId();
         }
     }
 
@@ -122,28 +74,6 @@ class Controller extends ComposerController
         $this->data['auth_user_id'] = $user ? $user->id : '';
     }
 
-    /**
-     * 查询同部门（角色）的数据
-     *
-     * @return void
-     */
-    public function queryDepartmentPermission()
-    {
-        if ($this->departmentPermission) {
-            $user = $this->getCurrentUser();
-            if (!($user['is_admin'] ?? '')) {
-                //非管理员  同部门的用户
-                $departmentId = $this->getUserDepartmentId($user);
-                if ($departmentId) {
-                    if ($this->departmentIdFieldIsJson) {
-                        $this->model = $this->model->whereJsonContains($this->departmentIdField, $departmentId);
-                    } else {
-                        $this->model = $this->model->where($this->departmentIdField, $departmentId);
-                    }
-                }
-            }
-        }
-    }
 
     /**
      * 获得当前登录的用户 设置缓存
@@ -158,71 +88,5 @@ class Controller extends ComposerController
             $user = Auth::user();
         }
         return $user;
-    }
-
-    /**
-     * 获得登录用户的部门（角色）ID
-     *
-     * @return void
-     */
-    public function getUserDepartmentId($user = '')
-    {
-        $departmentId = '';
-        if (!$user) {
-            $user = $this->getCurrentUser();
-        }
-        if ($user && $user->roles) {
-            $departmentId = $user->roles->first()->id;
-        }
-        return $departmentId;
-    }
-
-    /**
-     * 获得验证唯一字段的规则
-     *
-     * @param [type] $modelString
-     * @param [type] $validateField
-     * @return void
-     */
-    public function getUniqueFieldValidate($modelString, $validateField)
-    {
-        $departmentId = $this->getUserDepartmentId();
-        $currentUser = $this->getCurrentUser();
-        $isAdmin = $currentUser ? $currentUser->is_admin : 0;
-        $isJsonType = $this->departmentIdFieldIsJson;
-        return
-            tenant()->unique($modelString, $validateField)
-            ->when(!$isAdmin, function ($query) use ($departmentId, $isJsonType) {
-                if ($isJsonType) {
-                    $query->whereJsonContains($this->departmentIdField, $departmentId);
-                } else {
-                    $query->where($this->departmentIdField, $departmentId);
-                }
-            });
-    }
-
-    /**
-     * 验证更新数据
-     *
-     * @param [type] $modelString
-     * @param [type] $validateField
-     * @return void
-     */
-    public function getUpdateUniqueFieldValidate($modelString, $validateField)
-    {
-        $departmentId = $this->getUserDepartmentId();
-        $currentUser = $this->getCurrentUser();
-        $isAdmin = $currentUser ? $currentUser->is_admin : 0;
-        $isJsonType = $this->departmentIdFieldIsJson;
-        return
-            tenant()->unique($modelString, $validateField)
-            ->ignore($this->id)
-            ->when(!$isAdmin, function ($query) use ($departmentId, $isJsonType) {
-                if ($isJsonType) {
-                    $query->whereJsonContains($this->departmentIdField, $departmentId);
-                } else {
-                    $query->where($this->departmentIdField, $departmentId);
-                }
-            });
     }
 }
